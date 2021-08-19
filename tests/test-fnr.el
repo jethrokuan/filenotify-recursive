bd304347217d293625f62905a771fc553636d8bf
Jethro Kuan <jethrokuan95@gmail.com>
2021-08-19 12:52:28 +0800

    cleanup

--- tests/test-fnr.el
+++ tests/test-fnr.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2021 Jethro Kuan
 
 ;; Author: Jethro Kuan <jethrokuan95@gmail.com>
-;; Package-Requires: ((buttercup) (f.el))
+;; Package-Requires: ((buttercup))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 (require 'buttercup)
 (require 'filenotify-recursive)
 
-(defvar test-fnr-root-directory nil
+(defvar test-fnr--temp-testdir nil
   "Test root directory.")
 
 (defvar test-fnr-event-counter (make-hash-table :test 'equal)
@@ -31,7 +31,6 @@
 
 (defun test-fnr-counter-increment (f)
   "."
-  (message "HERE")
   (if-let ((count (gethash f test-fnr-event-counter)))
       (puthash f (1+ count) test-fnr-event-counter)
     (puthash f 1 test-fnr-event-counter)))
@@ -40,24 +39,19 @@
   "."
   (setq test-fnr-event-counter (make-hash-table :test 'equal)))
 
-(defun test-fnr-random-alnum ()
-  "."
-  (let* ((alnum "abcdefghijklmnopqrstuvwxyz0123456789")
-           (i (% (abs (random)) (length alnum))))
-      (substring alnum i (1+ i))))
-
-(defun test-fnr-random-string (n)
-  "Generate a slug of n random alphanumeric characters."
-  (if (= 0 n)
-      ""
-    (concat (test-fnr-random-alnum) (test-fnr-random-string (1- n)))))
+(defun fnr--make-temp-dir ()
+  "Create a temporary file directory for test."
+  (unless (stringp test-fnr--temp-testdir)
+    (setq test-fnr--temp-testdir
+          (expand-file-name
+           (make-temp-name "fnr-test") temporary-file-directory)))
+  (unless (file-directory-p test-fnr--temp-testdir)
+    (make-directory test-fnr--temp-testdir)))
 
 (defun test-fnr-create-test-directory ()
-  (let ((test-root (expand-file-name (format "%s/" (test-fnr-random-string 5)) temporary-file-directory)))
-    (make-directory test-root 'parents)
-    (make-directory (expand-file-name "a/a1" test-root) 'parents)
-    (make-directory (expand-file-name "b" test-root) 'parents)
-    (setq test-fnr-root-directory test-root)))
+  (fnr--make-temp-dir)
+  (make-directory (expand-file-name "a/a1" test-fnr--temp-testdir) 'parents)
+  (make-directory (expand-file-name "b" test-fnr--temp-testdir) 'parents))
 
 (defun test-fnr-touch (f)
      "Touches F."
@@ -71,26 +65,12 @@
     (fnr-clear-all))
 
   (it "correctly lists subdirectories"
-    (expect (mapcar #'directory-file-name (fnr--subdirectories-recursively test-fnr-root-directory))
+    (expect (mapcar #'directory-file-name (fnr--subdirectories-recursively test-fnr--temp-testdir))
             :to-have-same-items-as
-            (mapcar #'directory-file-name `(,test-fnr-root-directory
-                                           ,(expand-file-name "a" test-fnr-root-directory)
-                                           ,(expand-file-name "a/a1" test-fnr-root-directory)
-                                           ,(expand-file-name "b" test-fnr-root-directory)))))
-
-  (xit "correctly tracks items"
-    (let* ((id (fnr-add-watch test-fnr-root-directory
-                              '(change attribute-change) (lambda (e)
-                                                           (message "%s" e)
-                                                           (pcase e
-                                                             (`(,_ ,_ ,f)
-                                                              (test-fnr-counter-increment f))))))
-           (watcher (gethash id fnr-descriptors)))
-      (expect (length (fnr--watch-descs watcher)) :to-equal 4)
-      (let ((test-new-file (expand-file-name "a/foo" test-fnr-root-directory)))
-        (test-fnr-touch test-new-file)
-        (message "%s" test-fnr-event-counter)
-        (expect (gethash test-new-file test-fnr-event-counter) :to-equal 1)))))
+            (mapcar #'directory-file-name `(,test-fnr--temp-testdir
+                                           ,(expand-file-name "a" test-fnr--temp-testdir)
+                                           ,(expand-file-name "a/a1" test-fnr--temp-testdir)
+                                           ,(expand-file-name "b" test-fnr--temp-testdir))))))
 
 (provide 'test-fnr)
 ;;; test-fnr.el ends here
